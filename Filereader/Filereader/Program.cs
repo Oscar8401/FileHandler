a5d11cbbc23939166985b40a3bfe57ca5e95933e
Taleb01 <taleb_n1@hotmail.com>
2017-12-04 17:03:06 +0100

    Update json read method, LINQ

--- Filereader/Filereader/Program.cs
+++ Filereader/Filereader/Program.cs
@@ -1,9 +1,41 @@
 ﻿using System;
 using System.IO;
 using System.Xml;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Text;
+using System.Linq;
+using System.Web.Script.Serialization;
+//you should go to reference add reference and add system.web.extentions
+//otherwise system.web.script.serialization will not work
 
 namespace FileReader
 {
+    //creating class person to use it for json
+    class Person
+    {
+        public string firstName { get; set; }
+        public int age { get; set; }
+        public string lastName { get; set; }
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postalCode { get; set; }
+        public bool newSubscription { get; set; }
+        public string companyName { get; set; }
+        public override string ToString()
+        {
+            return string.Format("FirstName: {0} \nLastname: {1} \nAge: {2} \nStreetAddress: {3}" +
+                "\nCity: {4} \nState: {5} \nPostalCode: {6} \nNewSubscription: {7}" +
+                "\nCompanyName: {8}", firstName, lastName, age, streetAddress, city, state, postalCode, newSubscription, companyName);
+        }
+    }
+
+
+
+
+
+
     class Program
     {
         static void Main(string[] args)
@@ -12,49 +44,146 @@ namespace FileReader
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine("\n         Welcome user!");
-            bool userChoose = true;
 
-            while (userChoose)
+            bool desicion = true;
+            while (desicion)
             {
-                Console.WriteLine("You have three files in the libary" +
-                    "   \n1)TXT file" + "   \n2)JSON file" + "   \n3)XML fil2" + "   \n4)0 Exit the application");
-                var userInput = ' ';
+                Console.WriteLine("\nDo you want to read or to write a file?"
+                    + "\n1)Write" + "\n2)Read");
+                var userDesicion = ' ';
                 try
                 {
-                    userInput = Console.ReadLine()[0];
+                    userDesicion = Console.ReadLine()[0];
                 }
                 catch (IndexOutOfRangeException)
                 {
-                    Console.WriteLine("Please choose something! ");
+                    Console.WriteLine("Please choose something from the menu!");
                 }
-
-
-                switch (userInput)
+                switch (userDesicion)
                 {
+                    //submenu for the write version starts from here.
                     case '1':
-                        Console.BackgroundColor = ConsoleColor.DarkCyan;
-                        Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("TXT");
-                        Console.WriteLine("================================");
-                        TextReader();
-                        break;
-                    case '2':
                         Console.BackgroundColor = ConsoleColor.DarkMagenta;
                         Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Json");
+                        Console.WriteLine("Write");
                         Console.WriteLine("===================================");
-                        JsonRader();
+
+                        bool userValue = true;
+                        while (userValue)
+                        {
+                            Console.WriteLine("You can choose between three types of files"
+                                +"   \n1)XmlFile" + "   \n2)TxtFile" + "   \n3)JsonFile", "   \n4)0 Exit the application");
+                            var userwants = ' ';
+                            try
+                            {
+                                userwants = Console.ReadLine()[0];
+                            }
+                            catch (IndexOutOfRangeException)
+                            {
+                                Console.WriteLine("Please choose something from the menu");
+                            }
+                            switch (userwants)
+                            {
+                                case '1':
+                                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("Xml");
+                                    Console.WriteLine("================================");
+                                    XmlWriter();
+                                    break;
+                                case '2':
+                                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("Json");
+                                    Console.WriteLine("===================================");
+                                    JsonWriter();
+                                    break;
+                                case '3':
+                                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("XML");
+                                    Console.WriteLine("===================================");
+                                    break;
+                                case '0':
+                                    Console.Clear();
+                                    System.Threading.Thread.Sleep(1000);
+                                    return;
+                                default:
+                                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("Invalid Input. Try again! 0,1,2,3");
+                                    break;
+                            }
+
+                        }
                         break;
-                    case '3':
-                        Console.BackgroundColor = ConsoleColor.DarkGray;
-                        Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("XML");
-                        Console.WriteLine("===================================");
-                        XmlReader();
+                        //submenu for the read version starts from here.
+                    case '2':
+                        bool userChoose = true;
+
+                        while (userChoose)
+                        {
+                            Console.WriteLine("You have three files in the libary" +
+                                "   \n1)TXT file" + "   \n2)JSON file" + "   \n3)XML fil2" + "   \n4)0 Exit the application");
+                            var userInput = ' ';
+                            try
+                            {
+                                userInput = Console.ReadLine()[0];
+                            }
+                            catch (IndexOutOfRangeException)
+                            {
+                                Console.WriteLine("Please choose something from the menu");
+                            }
+
+
+                            switch (userInput)
+                            {
+                                case '1':
+                                    Console.BackgroundColor = ConsoleColor.DarkCyan;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("TXT");
+                                    Console.WriteLine("================================");
+                                    TextReader();
+                                    break;
+                                case '2':
+                                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("Json");
+                                    Console.WriteLine("===================================");
+                                    JsonRader();
+                                    break;
+                                case '3':
+                                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("XML");
+                                    Console.WriteLine("===================================");
+                                    XmlReader();
+                                    break;
+                                case '0':
+                                    Console.Clear();
+                                    System.Threading.Thread.Sleep(1000);
+                                    return;
+                                default:
+                                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.WriteLine("Invalid Input. Try again! 0,1,2,3");
+                                    break;
+                            }
+                            Console.ReadLine();
+                        }
                         break;
+
+
+
                     case '0':
                         Console.Clear();
                         System.Threading.Thread.Sleep(1000);
@@ -64,9 +193,8 @@ namespace FileReader
                         Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Black;
                         Console.WriteLine("Invalid Input. Try again! 0,1,2,3");
-                        break;
+                        return;
                 }
-                Console.ReadLine();
             }
         }
         /// <summary>
@@ -74,17 +202,31 @@ namespace FileReader
         /// </summary>
         static void JsonRader()
         {
+            //try
+            //{
+            //    string st = File.ReadAllText(@"C:\Users\User\Source\Repos\FileReader2\Filereader\Filereader\Files\json.json");
+            //    Console.WriteLine(st);
+            //}
+            //catch (Exception m)
+            //{
+            //    Console.WriteLine("Sorry the file is not available at the moment to read.");
+            //    Console.WriteLine(m.Message);
+            //}
+            //Console.Read();
             try
             {
-                string st = File.ReadAllText(@"C:\Users\User\Source\Repos\FileReader2\Filereader\Filereader\Files\json.json");
-                Console.WriteLine(st);
+                //Deserialize JSON from file
+                String JSONstring = File.ReadAllText(@"C:\Users\User\Source\Repos\FileReader2\Filereader\Filereader\Files\json.json");
+
+                JavaScriptSerializer ser = new JavaScriptSerializer();
+                Person p1 = ser.Deserialize<Person>(JSONstring);
+                Console.WriteLine(p1);
             }
             catch (Exception m)
             {
                 Console.WriteLine("Sorry the file is not available at the moment to read.");
                 Console.WriteLine(m.Message);
             }
-            Console.Read();
         }
         /// <summary>
         /// Read the file and display it line by line.
@@ -115,7 +257,8 @@ namespace FileReader
             Console.Read();
         }
         /// <summary>
-        /// 
+        ///  for more advance
+        ///  http://forum.codecall.net/topic/58239-c-tutorial-reading-and-writing-xml-files/
         /// </summary>
         static void XmlReader()
         {
@@ -133,5 +276,32 @@ namespace FileReader
                 Console.WriteLine(m.Message);
             }
         }
+        static void JsonWriter()
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            Person p2 = new Person() { firstName="taleb", age=32};
+            string outputJSON = ser.Serialize(p2);
+            File.WriteAllText("Output.json",outputJSON);
+        }
+        /// <summary>
+        /// http://csharp.net-informations.com/xml/csharp-xmltutorial.htm
+        /// </summary>
+        static void TextWriter() { }
+        /// <summary>
+        /// Creating a new XML file
+        /// </summary>
+        static void XmlWriter()
+        {
+            XDocument Tree = new XDocument(
+                 new XDeclaration("1.0","utf-8","yes"),
+                 new XComment("Creating an XML Tree using LINQ to XML"),
+                 new XElement("Student",
+                     new XElement("Student", new XAttribute("Id",101),
+                        new XElement("Name", "Mark"),
+                        new XElement("Gender", "Male"),
+                        new XElement("TotalMarks", "800"))));
+            Console.WriteLine(Tree);
+            Console.Read();
+        }   
     }
 }
