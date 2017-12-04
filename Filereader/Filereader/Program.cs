d21e2653e61f5935b9d356b5920996d6b080a869
Taleb01 <taleb_n1@hotmail.com>
2017-12-04 11:23:50 +0100

    Update the three read methods.

--- Filereader/Filereader/Program.cs
+++ Filereader/Filereader/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml;
 
 namespace FileReader
 {
@@ -15,7 +17,7 @@ namespace FileReader
             while (userChoose)
             {
                 Console.WriteLine("You have three files in the libary" +
-                    "   \n1)Json file" + "   \n2)XML file" + "   \n3)Txt file" + "   \n4)0 Exit the application");
+                    "   \n1)TXT file" + "   \n2)JSON file" + "   \n3)XML fil2" + "   \n4)0 Exit the application");
                 var userInput = ' ';
                 try
                 {
@@ -33,25 +35,25 @@ namespace FileReader
                         Console.BackgroundColor = ConsoleColor.DarkCyan;
                         Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Jaon file");
+                        Console.WriteLine("TXT");
+                        Console.WriteLine("================================");
+                        TextReader();
                         break;
                     case '2':
-                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.BackgroundColor = ConsoleColor.DarkMagenta;
                         Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("XML file");
+                        Console.WriteLine("Json");
+                        Console.WriteLine("===================================");
+                        JsonRader();
                         break;
                     case '3':
                         Console.BackgroundColor = ConsoleColor.DarkGray;
                         Console.Clear();
                         Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("Txt file");
-                        break;
-                    case '4':
-                        Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                        Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.Black;
-                        Console.WriteLine("case4");
+                        Console.WriteLine("XML");
+                        Console.WriteLine("===================================");
+                        XmlReader();
                         break;
                     case '0':
                         Console.Clear();
@@ -67,8 +69,69 @@ namespace FileReader
                 Console.ReadLine();
             }
         }
-        static void JsonRader() { }
-        static void TextReader() { }
-        static void XmlReader() { }
+        /// <summary>
+        /// 
+        /// </summary>
+        static void JsonRader()
+        {
+            try
+            {
+                string st = File.ReadAllText(@"C:\Users\User\Source\Repos\FileReader2\Filereader\Filereader\Files\json.json");
+                Console.WriteLine(st);
+            }
+            catch (Exception m)
+            {
+                Console.WriteLine("Sorry the file is not available at the moment to read.");
+                Console.WriteLine(m.Message);
+            }
+            Console.Read();
+        }
+        /// <summary>
+        /// Read the file and display it line by line.
+        /// then tells the user how many lines are in the file
+        /// </summary>
+        static void TextReader()
+        {
+            //Making sure that the file exist.
+            try
+            {
+                int counter = 0;
+                string line;
+                StreamReader file = new StreamReader(@"C:\Users\User\Source\Repos\FileReader2\Filereader\Filereader\Files\TextFile1.txt");
+                while ((line = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine("");
+                Console.WriteLine("///////////////////////////There were {0} lines.///////////////////////////////", counter);
+             }
+            catch (Exception m)
+            {
+                Console.WriteLine("Sorry the file is not available at the moment");
+                Console.WriteLine(m.Message);
+            }
+            Console.Read();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        static void XmlReader()
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                string file =  @"C:\Users\User\Source\Repos\FileReader2\Filereader\Filereader\Files\XMLFile.xml";
+                xmlDoc.Load(file);
+                xmlDoc.Save(Console.Out);
+                Console.Read();
+            }
+            catch (Exception m)
+            {
+                Console.WriteLine("Sorry the file is not available at the moment.");
+                Console.WriteLine(m.Message);
+            }
+        }
     }
 }
